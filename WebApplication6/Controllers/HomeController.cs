10866790281968e1a659be1cfa9c540926c8f2a5
Michał Kapela <michal.kapela@yahoo.com>
2018-01-28 17:30:55 +0100

    start

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Results()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Tournament()
+        {
+
+            return View();
+        }
+
+        public ActionResult Players()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Horses()
+        {
+            
+            return View();
+        }
+    }
+}
